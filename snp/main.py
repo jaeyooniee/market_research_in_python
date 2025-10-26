66abd2f7b0089fe0adf5195ab241fcc8dac6072f
Jaeyoon Chung <jaeyoon.chung@student.manchester.ac.uk>
2025-10-26 21:03:56 +0000

    Added Results, Personal Research and Explanation

--- snp/main.py
+++ snp/main.py
@@ -1,18 +1,87 @@
-"""
-Compare rolling win rates of S&P 100 vs S&P 500 (Custom Day Window and Step)
-"""
+
+#Compare rolling win rates of S&P 100 vs S&P 500 (Custom Day Window and Step)
 
 """
+Program results:
+
+start_date     end_date
 1995-01-01 -> 1999-12-31 (window_days=365, step_days=7): win_rate=34/52 (65.38%) - Dotcom Bubble
 2000-01-01 -> 2004-12-31 (window_days=365, step_days=7): win_rate=17/52 (32.69%)
-2005-01-01 -> 2009-12-31 (window_days=365, step_days=7): win_rate=17/52 (32.69%)
+2005-01-01 -> 2009-12-31 (window_days=365, step_days=7): win_rate=17/52 (32.69%) - Bearish market
 2010-01-01 -> 2014-12-31 (window_days=365, step_days=7): win_rate=23/52 (44.23%)
 2015-01-01 -> 2019-12-31 (window_days=365, step_days=7): win_rate=28/52 (53.85%)
 2020-01-01 -> 2024-12-31 (window_days=365, step_days=7): win_rate=34/52 (65.38%) - AI bubble(?)
 
-What kind of companies led S&P100 to outperform S&P500? What was the difference between 100 and 500?
-Why did this happen? What's the similarity between 1995-2000 and 2020-2025?
+
+Personal Research:
+
+- From 1995-1999, the annual return from investing S&P500 index was 34.11%, 20.26%, 31.01%, 26.67% and 19.53%.
+These numbers can be considered as decent returns when it comes to market indices. Markets in 1995-1999 were actually 
+amazingly bullish. 
+
+- In mid to late 1990s, traditional industries such as energy, finance, industrials and consumer goods had a large presence. 
+Technology stocks existed but were not yet dominant. (e.g. Exxon Mobil, Coca-cola, General Electric, Merck, P&G and so on)
+
+- There were also changes in the number of companies per sector and shifts in sector leadership between 1995-1999.
+
+- Technology now occupies a far larger share of the index than it did in the past.
+In the early 2000s, IT accounted for about 20-25% of the index, whereas today its share and concentration among 
+top companies are significantly higher (currently 35%).
+
+- In the past, companies tended to remain in the index for decades. 
+Today, due to technological disruption, M&A, and slower growth, turnover is much more frequent.
+
+- In 1995, S&P 500 sector weights were fairly balanced: 
+The largest sector (Consumer Discretionary) had ~15% and The smallest (Utilities) had ~5%.
+
+- Today, sector distribution has become much more uneven:
+IT services now dominates with ~25% and Telecommunications has fallen to around 2%.
+
+- This represents a systematic and steady shift over the past few decades, 
+where market capitalization has become concentrated in fewer, dominant sectors, especially Financials and IT.
+
+- Actually, this concentration brings back 2000 (the Dotcom bubble), when tech valuations similarly dominated the index.
+
+- The index these days is far more concentrated than in 1995, with only a few sectors driving the majority of total returns.
+
+- Today's S&P 500 is not a broadly diversified representation of the economy, 
+but rather heavily dependent on a few sectors and mega-cap firms.
+
+source: https://www.tfginvest.com/insights/the-impact-of-the-sp-tech-industry-concentration
+source: https://einvestingforbeginners.com/historical-sp-500-industry-weights-20-years
+
+
+Q1. What kind of companies led S&P100 to outperform S&P500? 
+A1. In 5 years time gap, it was 1995-1999 and 2020-2024 when S&P100 index beated S&P500 index. 
+Those moments continued due to widely known 'Dotcom Bubble' at 2000 and huge AI investment from big techs which is still 
+an ongoing event. When we list the firms that led the stock market at the moment, there are Microsoft, General Electric, Intel, 
+Cisco Systems, IBM, Exxon Mobil from 1995 to 1999 and Apple, Microsoft (again), Amazon, Alphabet, NVIDIA, Meta, Broadcom from 
+2020 ~. The results indicates that TECH COMPANIES were always with the market when the bubble was getting (seemingly) endlessly big
+and these few tech firms eventually affected the increase of the market indices to a meaningful extent.
+
+
+Q2. Why did this happen? What's the similarity between 1995-2000 and 2020-2025?
+A2. As mentioned above, both the late 1990s and early 2020s experienced powerful bull markets driven by 
+technological revolutions and abundant liquidity. During 1995-2000, optimism around the Internet and personal computing 
+fueled soaring valuations, while from 2020-2025, the rise of artificial intelligence, cloud computing, and semiconductors 
+produced a similar surge in investor enthusiasm. In both periods, easy monetary policy and cheap capital encouraged speculation, 
+leading to extreme market concentration where a handful of dominant firms drove most of the index's returns.
+Investor psychology also played a key role, as widespread belief in a “new era” of growth created momentum-driven rallies 
+detached from fundamentals. Ultimately, both eras illustrate how technological transformation
+can reshape market structures and make the S&P 500 heavily reliant on just a few mega-cap companies.
+
+
+
+Q3. As long as IT and Computing industries develops, will this trend last forever?
+A3. The answer is no. There have been numerous shifts all over the world for few centuries (Agriculture -> Mechanics -> 
+Energy -> Hardware & Software -> Artificial Intelligence & Robotics -> ?) and a number of expectation in few decades contain 
+different aspects in which the leading industry will take human beings to next level. Getting rid of former machines and
+technologies makes no sense, however, it'll likely to be disappear by itself or hardly be used in future according to human history 
+of revolution so far. Human will find an answer to improve the possible downsides of current technologies and open a new era.
+
+
 """
+# the code starts here
 
 import pandas as pd
 import matplotlib.pyplot as plt
